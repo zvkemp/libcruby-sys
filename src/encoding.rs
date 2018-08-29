a3eb23f3d9b1eec4c114783504ff8911d36ec50b
Peter Wagenet <peter.wagenet@gmail.com>
2018-08-29 15:16:39 -0700

    Add `rb_utf8_encindex`
    Is it a macro? NO
    
    Is there a macro version? NO
    
    Defined in:
    
    2.3 https://github.com/ruby/ruby/blob/v2_3_7/include/ruby/encoding.h#L261
    2.4 https://github.com/ruby/ruby/blob/v2_4_4/include/ruby/encoding.h#L265
    2.5 https://github.com/ruby/ruby/blob/v2_5_1/include/ruby/encoding.h#L265
    2.6 https://github.com/ruby/ruby/blob/v2_6_0_preview2/include/ruby/intern.h#L269

--- src/encoding.rs
+++ src/encoding.rs
@@ -3,6 +3,7 @@ use libc::c_int;
 
 extern {
     pub fn rb_enc_get_index(obj: VALUE) -> c_int;
+    pub fn rb_utf8_encindex() -> c_int;
 }
 
 tests! {
@@ -12,5 +13,10 @@ tests! {
     #[test]
     fn test_enc_get_index(assert: &mut Assertions) {
         assert.rs_eq(unsafe { rb_enc_get_index("foo".to_ruby()) } > 0, true);
+    }
+
+    #[test]
+    fn test_rb_utf8_encindex(assert: &mut Assertions) {
+        assert.rs_eq(unsafe { rb_enc_get_index("foo".to_ruby()) }, unsafe { rb_utf8_encindex() });
     }
 }
